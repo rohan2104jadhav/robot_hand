918d8fd3d9424c92d04bc25276923e5e676a846e
Rohan <rohan2104jadhav@example.com>
2026-03-25 21:36:28 +0530

    Restructure project with src/ folder and ROS2 package

--- src/arm/launch/display.left.py
+++ src/arm/launch/display.left.py
@@ -7,7 +7,7 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    pkg_path = get_package_share_directory("wujihand-urdf")
+    pkg_path = get_package_share_directory("arm")
     urdf_file = os.path.join(pkg_path, "urdf", "left.urdf")
 
     use_sim_time = LaunchConfiguration("use_sim_time", default="false")
