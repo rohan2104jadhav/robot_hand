20b68ab044f10d8ae8dac18999048aa095662015
Rohan <rohan2104jadhav@example.com>
2026-07-01 00:16:02 +0530

    add more files

--- src/arm/launch/display.left.py
+++ src/arm/launch/display.left.py
@@ -51,7 +51,7 @@ def generate_launch_description():
             DeclareLaunchArgument("use_sim_time", default_value="false"),
             DeclareLaunchArgument("gui", default_value="true"),
             robot_state_publisher_node,
-            # joint_state_publisher_gui_node,
+            joint_state_publisher_gui_node,
             rviz_node,
             hand_operation_node,
         ]
